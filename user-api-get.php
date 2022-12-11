f8f48975c56fb3a462948d9499097de7ee654c10
Swati <swati.kokate008@gmail.com>
2022-12-11 20:10:48 +0530

    Add create API

--- user-api-get.php
+++ user-api-get.php
@@ -5,21 +5,21 @@ $db_connection = mysqli_connect("localhost", "root", "", "test");
 
 // insert into user (first_name, last_name, email, phone_number) values ('Swati', 'Kokate', 'sk@gmail.com', '1234567890');
 
-$users = get_users($db_connection);
+$users = get_assoc_array($db_connection, "select first_name, last_name, email, phone_number from user");
 echo json_encode($users);
 
 
 
 
 
-function get_users($db_connection)
+function get_assoc_array($db_connection, $query)
 {
-    $results = $db_connection->query("select first_name, last_name, email, phone_number from user");
-    $users = array();
+    $results = $db_connection->query($query);
+    $records = array();
 
     while ($row = $results->fetch_assoc()) {
-        array_push($users, $row);
+        array_push($records, $row);
     }
 
-    return $users;
+    return $records;
 }
