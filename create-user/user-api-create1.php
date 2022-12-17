4e2d03de72533ff176f2954e30608ce26108ec10
Swati <swati.kokate008@gmail.com>
2022-12-17 12:41:36 +0530

    Add create user

---
+++ create-user/user-api-create1.php
@@ -0,0 +1,17 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once "db-utils.php";
+
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$phone_number = $_POST['phone_number'];
+
+$user_insert_query = "insert into user (first_name, last_name, email, phone_number) values ('" . $first_name . "' ,'" . $last_name . "', '" . $email . "', '" . $phone_number . "')";
+
+if (save($user_insert_query) === TRUE) {
+    echo json_encode(array("status" => "success"));
+} else {
+    echo json_encode(array("status" => "error"));
+}
